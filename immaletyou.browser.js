776c2a0c13a47fd32fcf13f97cb903c6076ec15a
Justin Martin <justin.martin@corda.com>
2014-01-21 13:19:27 -0700

    Up the npm version to 0.1.3.  Add some more config options.

--- immaletyou.browser.js
+++ immaletyou.browser.js
@@ -6,11 +6,13 @@
  **/
 
 var obj = (function() {
-  var mocks, modules = {};
+  var mocks, modules = {}, testsBasePathRegex = '', testsExtensionReplaceRegex = '\.js\?.*';
 
   ImmaLetYou = function(opts) {
     this.basePath = opts.basePath || '';
     this.defaultMock = opts.defaultMock || {};
+    testsBasePathRegex = opts.testsBasePathRegex || testsBasePathRegex;
+    testsExtensionReplaceRegex = opts.testsExtensionReplaceRegex || testsExtensionReplaceRegex;
   };
 
   ImmaLetYou.prototype.finish = function(path) {
@@ -50,8 +52,8 @@ var obj = (function() {
     var scripts = document.getElementsByTagName('script');
     var last = scripts[scripts.length - 1];
     var fullPath = last.src;
-    var requirePath = fullPath.replace(/http:\/\/.*\/base\//g,'');
-    requirePath = requirePath.replace(/\.js\?.*/g,'');
+    var requirePath = fullPath.replace(new RegExp('http:\/\/.*\/' + testsBasePathRegex,'g'),'');
+    requirePath = requirePath.replace(new RegExp(testsExtensionReplaceRegex, 'g'),'');
     modules[requirePath] = callback;
   };
   return {
